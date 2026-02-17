115b9894ca81046c4fa5a98f26ae5cdacd870428
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-17 15:10:42 +0200

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,16 @@
+'use strict';
+// write code here
+const inputs = [
+    ...document.querySelectorAll('form input')
+];
+for (const input of inputs){
+    const label = document.createElement('label');
+    const capitalizedName = input.name[0].toUpperCase() + input.name.slice(1);
+    label.classList.add('field-label');
+    label.htmlFor = input.id;
+    label.textContent = capitalizedName;
+    input.placeholder = capitalizedName;
+    input.parentNode.append(label);
+}
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
