9c16622d1ec915c56b1b3508362c8034a5ef2da6
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-17 14:53:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const surrender = input.name[0].toUpperCase() + input.name.slice(1);
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = surrender;
+
+  input.placeholder = surrender;
+  input.parentNode.append(label);
+}
